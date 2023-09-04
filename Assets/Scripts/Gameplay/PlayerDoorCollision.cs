2d6de20f3a973936b2b83fd19d639ed105ab8f92
BenP <ben.price2013@gmail.com>
2023-09-04 16:43:44 -0500

    Add a simple door object that changes scene on collision

---
+++ Assets/Scripts/Gameplay/PlayerDoorCollision.cs
@@ -0,0 +1,28 @@
+using System;
+using Platformer.Core;
+using Platformer.Mechanics;
+using Platformer.Model;
+using UnityEngine;
+using static Platformer.Core.Simulation;
+
+namespace Platformer.Gameplay
+{
+
+    /// <summary>
+    /// Fired when a Player collides with an door.
+    /// </summary>
+    /// <typeparam name="doorCollision"></typeparam>
+    public class PlayerDoorCollision : Simulation.Event<PlayerDoorCollision>
+    {
+        public DoorController door;
+        public PlayerController player;
+
+        PlatformerModel model = Simulation.GetModel<PlatformerModel>();
+
+        public override void Execute()
+        {
+            door.sceneChanger.ChangeScene(door.sceneName);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Mechanics/DoorController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Platformer.Gameplay;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace Platformer.Mechanics 
+{
+using static Platformer.Core.Simulation;
+    [RequireComponent(typeof(Collider2D))]
+    public class DoorController : MonoBehaviour
+    {
+        internal Collider2D _collider;
+        SpriteRenderer spriteRenderer;
+
+        public Bounds Bounds => _collider.bounds;
+        public SceneChanger sceneChanger;
+        public string sceneName;
+
+        void Awake()
+        {
+            _collider = GetComponent<Collider2D>();
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            var player = collision.gameObject.GetComponent<PlayerController>();
+            if (player != null)
+            {
+                var ev = Schedule<PlayerDoorCollision>();
+                ev.player = player;
+                ev.door = this;
+            }
+        }
+
+        void Update()
+        {
+        }
+
+    }
+}
