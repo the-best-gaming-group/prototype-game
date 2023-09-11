995ca15c4c9ae099ff8a1fc660f579ba69f3e199
BenP <ben.price2013@gmail.com>
2023-09-10 22:00:55 -0500

    Get a ghost to move side to side and do nothing else lol

---
+++ Assets/Scripts/Mechanics/GhostController.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Platformer.Gameplay;
+using static Platformer.Core.Simulation;
+using Platformer.Model;
+using Platformer.Core;
+
+namespace Platformer.Mechanics
+{
+    /// <summary>
+    /// This is the main class used to implement control of the player.
+    /// It is a superset of the AnimationController class, but is inlined to allow for any kind of customisation.
+    /// </summary>
+    public class GhostController : KinematicObject3D
+    {
+        public AudioClip jumpAudio;
+        public AudioClip respawnAudio;
+        public AudioClip ouchAudio;
+
+        /// <summary>
+        /// Max horizontal speed of the player.
+        /// </summary>
+        public float maxSpeed = 7;
+        /// <summary>
+        /// Initial jump velocity at the start of a jump.
+        /// </summary>
+        public float jumpTakeOffSpeed = 7;
+
+        public JumpState jumpState = JumpState.Grounded;
+        private bool stopJump;
+        internal new Collider collider;
+        /*internal new*/ public AudioSource audioSource;
+        public bool controlEnabled = true;
+
+        bool jump;
+        Vector3 move;
+        readonly PlatformerModel model = Simulation.GetModel<PlatformerModel>();
+
+        public Bounds Bounds => collider.bounds;
+
+        void Awake()
+        {
+            audioSource = GetComponent<AudioSource>();
+            collider = GetComponent<Collider>();
+            if (GlobalData.playerHealth == null) {
+                GlobalData.InitHealth();
+            }
+            Debug.Log("Player health is: " + GlobalData.playerHealth.currentHP.ToString());
+        }
+
+        protected override void Update()
+        {
+            if (controlEnabled)
+            {
+                move.x = Input.GetAxis("Horizontal");
+                if (jumpState == JumpState.Grounded && Input.GetButtonDown("Jump"))
+                    jumpState = JumpState.PrepareToJump;
+                else if (Input.GetButtonUp("Jump"))
+                {
+                    stopJump = true;
+                }
+            }
+            else
+            {
+                move.x = 0;
+            }
+            UpdateJumpState();
+            base.Update();
+        }
+
+        void UpdateJumpState()
+        {
+            jump = false;
+            switch (jumpState)
+            {
+                case JumpState.PrepareToJump:
+                    jumpState = JumpState.Jumping;
+                    jump = true;
+                    stopJump = false;
+                    break;
+                case JumpState.Jumping:
+                    if (!IsGrounded)
+                    {
+                        jumpState = JumpState.InFlight;
+                    }
+                    break;
+                case JumpState.InFlight:
+                    if (IsGrounded)
+                    {
+                        jumpState = JumpState.Landed;
+                    }
+                    break;
+                case JumpState.Landed:
+                    jumpState = JumpState.Grounded;
+                    break;
+            }
+        }
+
+        protected override void ComputeVelocity()
+        {
+            if (jump && IsGrounded)
+            {
+                velocity.y = jumpTakeOffSpeed * model.jumpModifier;
+                jump = false;
+            }
+            else if (stopJump)
+            {
+                stopJump = false;
+                if (velocity.y > 0)
+                {
+                    velocity.y = velocity.y * model.jumpDeceleration;
+                }
+            }
+
+            targetVelocity = move * maxSpeed;
+        }
+
+        public enum JumpState
+        {
+            Grounded,
+            PrepareToJump,
+            Jumping,
+            InFlight,
+            Landed
+        }
+    }
+}
---
+++ Assets/Scripts/Mechanics/KinematicObject3D.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Platformer.Mechanics
+{
+    /// <summary>
+    /// Implements game physics for some in game entity.
+    /// </summary>
+    public class KinematicObject3D : MonoBehaviour
+    {
+        /// <summary>
+        /// The minimum normal (dot product) considered suitable for the entity sit on.
+        /// </summary>
+        public float minGroundNormalY = .65f;
+
+        /// <summary>
+        /// A custom gravity coefficient applied to this entity.
+        /// </summary>
+        public float gravityModifier = 0f;
+
+        /// <summary>
+        /// The current velocity of the entity.
+        /// </summary>
+        public Vector3 velocity;
+
+        /// <summary>
+        /// Is the entity currently sitting on a surface?
+        /// </summary>
+        /// <value></value>
+        public bool IsGrounded { get; private set; }
+
+        protected Vector3 targetVelocity;
+        protected Vector3 groundNormal = new Vector3(0, -1, 0);
+        protected Rigidbody body;
+        protected RaycastHit[] hitBuffer = new RaycastHit[16];
+
+        protected const float minMoveDistance = 0.001f;
+        protected const float shellRadius = 0.01f;
+
+
+        /// <summary>
+        /// Bounce the object's vertical velocity.
+        /// </summary>
+        /// <param name="value"></param>
+        public void Bounce(float value)
+        {
+            velocity.y = value;
+        }
+
+        /// <summary>
+        /// Bounce the objects velocity in a direction.
+        /// </summary>
+        /// <param name="dir"></param>
+        public void Bounce(Vector3 dir)
+        {
+            velocity.y = dir.y;
+            velocity.x = dir.x;
+        }
+
+        /// <summary>
+        /// Teleport to some position.
+        /// </summary>
+        /// <param name="position"></param>
+        public void Teleport(Vector3 position)
+        {
+            body.position = position;
+            velocity *= 0;
+            body.velocity *= 0;
+        }
+
+        protected virtual void OnEnable()
+        {
+            body = GetComponent<Rigidbody>();
+            body.isKinematic = true;
+        }
+
+        protected virtual void OnDisable()
+        {
+            body.isKinematic = false;
+        }
+
+        protected virtual void Start()
+        {
+        }
+
+        protected virtual void Update()
+        {
+            targetVelocity = Vector3.zero;
+            ComputeVelocity();
+        }
+
+        protected virtual void ComputeVelocity()
+        {
+
+        }
+
+        protected virtual void FixedUpdate()
+        {
+            //if already falling, fall faster than the jump speed, otherwise use normal gravity.
+            if (velocity.y < 0)
+                velocity += gravityModifier * Physics.gravity * Time.deltaTime;
+            else
+                velocity += Physics.gravity * Time.deltaTime;
+
+            velocity.x = targetVelocity.x;
+            velocity.y = 0;
+
+            IsGrounded = false;
+
+            var deltaPosition = velocity * Time.deltaTime;
+
+            Debug.Log(groundNormal);
+            var moveAlongGround = new Vector3(groundNormal.y, -groundNormal.x);
+
+            var move = moveAlongGround * deltaPosition.x;
+
+            PerformMovement(move, false);
+
+            move = Vector3.up * deltaPosition.y;
+
+            PerformMovement(move, true);
+
+        }
+
+        void PerformMovement(Vector3 move, bool yMovement)
+        {
+            var distance = move.magnitude;
+
+            if (distance > minMoveDistance)
+            {
+                //check if we hit anything in current direction of travel
+            }
+            body.position = body.position + move.normalized * distance;
+        }
+
+    }
+}
